eead7c11a453c1194be60aa8881af26c0fb4ab18
Mark Vasilkov <mvasilkov@gmail.com>
2014-06-04 19:41:24 +0300

    Changes

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 var express = require('express')
-var app = express()
+var morgan  = require('morgan')
+var favicon = require('serve-favicon')
+var app     = express()
+
+app.enable('strict routing')
+app.disable('x-powered-by')
+
+app.use(morgan('dev'))
+app.use(favicon(__dirname + '/pub/favicon.ico'))
+app.use(express.static(__dirname + '/pub'))
+
 var port = process.env.PORT || 8086
 var server = app.listen(port, function () {
     console.log('POMF =3 on port %d', server.address().port)
