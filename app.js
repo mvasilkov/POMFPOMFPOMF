606d4259777cb217e8c95957f2a35d090fee8f29
Mark Vasilkov <mvasilkov@gmail.com>
2014-06-07 12:16:38 +0300

    Changes

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 var express = require('express')
 var logger  = require('morgan')
-var favicon = require('serve-favicon')
 var app     = express()
 
 app.enable('strict routing')
 app.disable('x-powered-by')
 app.use(logger('dev'))
 
-app.get('/index.html', function (req, res) {
-    res.redirect(301, '/')
-})
+app.get('/index.html', function (req, res) { res.redirect(301, '/') })
 
-app.use(favicon(__dirname + '/pub/favicon.ico'))
 app.use(express.static(__dirname + '/pub'))
+
+app.use(function (req, res) { res.sendfile(__dirname + '/pub/index.html') })
 
 function run() {
     var port = process.env.PORT || 8086
